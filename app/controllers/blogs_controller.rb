2cc223a2c5108b4a8c3d650220fa3c1af0eaeec8
TAIKI-desuyo <tyki0673@gmail.com>
2021-09-12 08:59:56 +0000

    [finish]2-8

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -11,11 +11,14 @@ class BlogsController < ApplicationController
   def new
     @blog = Blog.new
   end
-
+  
   def create
-    blog = Blog.new(blog_params)
-    blog.save
-    redirect_to blogs_path
+    @blog = Blog.new(blog_params)
+   if @blog.save
+    redirect_to blog_path(@blog.id)
+   else 
+     render :new
+   end
   end
 
 
